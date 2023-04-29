e2d6729bbfacd296ae03e627d80d9c08136c972f
CyberTitus <82715493+CyberTitus@users.noreply.github.com>
2023-04-29 14:51:03 +0100

    Initial

--- app.py
+++ app.py
@@ -1,23 +1,19 @@
-from flask import Flask, flash, request, render_template, redirect
-from short import *
-from forms import *
+from flask import Flask, render_template, request
+import requests
+app = Flask(__name__)
 
-app=Flask(__name__)
-app.secret_key ='B\x04\x8b\xb7\x00\xb1\x02.K[\xe2XB\x04\x8b\xb7\x00\xb1\x02.K[\xe2X'
-csrf = CSRFProtect()
-csrf.init_app(app)
+def shortify(URL):
+	res = requests.get(f'https://api.shrtco.de/v2/shorten?url={URL}').json()['result']
+	return (res['full_short_link'], res['full_short_link2'], res['full_short_link3'])
 
+@app.get('/')
+def index():
+	return render_template('index.html')
 
-@app.route('/', methods=['GET', 'POST'])
-def main():
-	form = subMit()
-	if request.method == 'POST':
-		link = request.form['url']
-		shorted_urls = shortURL(link)
-		flash('Shorted URLs Successfully!','success')
-		return render_template('index.html', shorted_urls=shorted_urls, form=form)
-	return render_template('index.html',form=form)
-
+@app.post('/')
+def ipost():
+	link = request.form['link']
+	return render_template('index.html', links=shortify(link))
 
 if __name__ == '__main__':
 	app.run()
--- forms.py
+++
@@ -1,5 +0,0 @@
-from flask_wtf import FlaskForm, CSRFProtect
-from wtforms import SubmitField
-
-class subMit(FlaskForm):
-	submit = SubmitField('Shorten')
